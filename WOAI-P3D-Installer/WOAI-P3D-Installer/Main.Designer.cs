1b58827b278b0d197670c22277164710b2fb5e02
Wills Bithrey <wills@willsbithrey.com>
2016-02-02 07:08:26 +0000

    Initial UI

---
+++ WOAI-P3D-Installer/WOAI-P3D-Installer/Main.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WOAI_P3D_Installer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbdPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnChooseFolder = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChooseFolder
+            // 
+            this.btnChooseFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseFolder.Location = new System.Drawing.Point(30, 96);
+            this.btnChooseFolder.Name = "btnChooseFolder";
+            this.btnChooseFolder.Size = new System.Drawing.Size(144, 23);
+            this.btnChooseFolder.TabIndex = 0;
+            this.btnChooseFolder.Text = "Choose Folder";
+            this.btnChooseFolder.UseVisualStyleBackColor = true;
+            this.btnChooseFolder.Click += new System.EventHandler(this.btnChooseFolder_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(30, 70);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(295, 20);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "G:\\Windows 10 (Sim)\\Users\\Wills\\Documents\\WOAI Processor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Base Folder";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(181, 96);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(144, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslStatus,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 187);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(359, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(28, 17);
+            this.toolStripStatusLabel1.Text = "v1.0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(359, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.aboutToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbProgress.Location = new System.Drawing.Point(30, 141);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(295, 23);
+            this.pbProgress.Step = 1;
+            this.pbProgress.TabIndex = 7;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(138, 17);
+            this.toolStripStatusLabel2.Spring = true;
+            // 
+            // tsslStatus
+            // 
+            this.tsslStatus.Name = "tsslStatus";
+            this.tsslStatus.Size = new System.Drawing.Size(39, 17);
+            this.tsslStatus.Text = "Ready";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(129, 240);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 8;
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(138, 17);
+            this.toolStripStatusLabel3.Spring = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 209);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.pbProgress);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnChooseFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "WOAI P3D Installer";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdPath;
+        private System.Windows.Forms.Button btnChooseFolder;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.ProgressBar pbProgress;
+        private System.Windows.Forms.ToolStripStatusLabel tsslStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ProgressBar progressBar2;
+    }
+}
+
---
+++ WOAI-P3D-Installer/WOAI-P3D-Installer/Main.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WOAI_P3D_Installer
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseFolder_Click(object sender, EventArgs e)
+        {
+            if (fbdPath.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = fbdPath.SelectedPath;
+            }
+        }
+
+        private bool checkFolderStructure()
+        {
+            string sourceRootDirectory = Path.Combine(this.txtPath.Text, "Source");
+            string extractedPackagesDirectory = Path.Combine(this.txtPath.Text, "Source\\Extracted Packages");
+            string outputRootDirectory = Path.Combine(this.txtPath.Text, "Output");
+
+            // Check that "<ROOT>\Source\Extracted Packages" exists
+            if (File.Exists(extractedPackagesDirectory))
+            {
+                DialogResult dialogResult = MessageBox.Show("Could not find directory: " + extractedPackagesDirectory +
+                    "\nWould you like me to create it for you?", "Missing Directory", MessageBoxButtons.OKCancel, 
+                    MessageBoxIcon.Question);
+
+                switch (dialogResult) {
+                    case DialogResult.OK:
+                        Directory.CreateDirectory(extractedPackagesDirectory);
+                        break;
+                    case DialogResult.Cancel:
+                        MessageBox.Show("One or more directory checks failed. Please create the proper directory " + 
+                            "structure manually and then retry", "Process Aborted", MessageBoxButtons.OK, 
+                            MessageBoxIcon.Information);
+                        return false;
+                }
+            }
+
+            // Check that the output folder exists and is empty.
+            if (File.Exists(extractedPackagesDirectory))
+            {
+                if (Directory.EnumerateFileSystemEntries(outputRootDirectory).Any())
+                {
+                    DialogResult dialogResult = MessageBox.Show("The Output directory is not empty. Continuing may " +
+                        "overwrite previously processed files. Are you sure you want to continue?", "Question", 
+                        MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+                    if (dialogResult == DialogResult.Cancel)
+                    {
+                        MessageBox.Show("OK - No files have been overwritten. Please rerun the process once you are" +
+                                " happy that the directory is empty or that the contents may be overwritten", 
+                                "Process Aborted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return false;
+                    }
+                }
+                
+            } else {
+                MessageBox.Show("Extracted Packages directory does not exist.", "Error", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+
+            return true;
+        }
+
+        private void copyGlobalTextures()
+        {
+            
+        }
+
+        private void moveAircraft()
+        {
+
+        }
+
+        private void copyBgls()
+        {
+
+        }
+
+        private void resetUi(bool retainProgress) {
+            this.unlockUi();
+
+            if (!retainProgress) {
+                pbProgress.Value = 0;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            bool folderCheckOk;
+
+            this.lockUi();
+            this.updateProgress(1);
+
+            folderCheckOk =  this.checkFolderStructure();
+
+            if (!folderCheckOk) {
+                this.resetUi(false);
+                return;
+            }
+
+            this.updateProgress(5);
+            this.updateProgress(50);
+        }
+
+        private void lockUi()
+        {
+            this.txtPath.Enabled = false;
+            this.btnChooseFolder.Enabled = false;
+            this.btnGo.Enabled = false;
+        }
+
+        private void unlockUi()
+        {
+            this.txtPath.Enabled = true;
+            this.btnChooseFolder.Enabled = true;
+            this.btnGo.Enabled = true;
+        }
+
+        private void updateProgress(int value)
+        {
+            this.pbProgress.Value = value;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("WOAI Installer for P3D - v1.0\nCreated by " + 
+                "Wills Bithrey\nPlease file issues on GitHub at " + 
+                "https://github.com/WillsB3/WOAI-P3D-Installer", "About", 
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
